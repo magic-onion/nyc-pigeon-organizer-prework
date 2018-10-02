74e14cd843b387f4ca4619a977da2531b72eb887
Alexander Truong <alexander.m.truong@gmail.com>
2018-10-02 18:15:59 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,29 +17,33 @@ pigeon_data = {
     "City Hall" => ["Andrew"]
   }
 }
+
 def nyc_pigeon_organizer(data)
- new_hash = {}
-   # Get array of unique names
-  pigeon_names = data.collect do |attributes, details|
-    details.values
+  pigeon_list = {}
+  #create an array of unique names
+  pigeon_names = []
+    data.each do |key, value|
+      pigeon_names << value.values
+    end
+  pigeon_names.flatten.uniq
+ 
+  pigeon_names.each do |name|
+    pigeon_list[name] = {}
   end
-  .flatten.uniq
-   # Create hash with each name as key
-  # {"Theo"=> {}, "Peter" => {},...}
-  pigeon_names.each {|name| new_hash[name] = {}}
-   # Create keys(categories for each pigeon) with value of empty array
-  data.each do |attributes, details|
-    new_hash.each do |name, hash|
-      new_hash[name][attributes] = []
+  
+  data.each do |key, value|
+    pigeon_list.each do |name, hash|
+      pigeon_list[name][key] = []
     end
   end
-   # Assign info to each key
-  data.each do |attribute, details|
-    details.each do |property, names|
-      names.each do |name|
-        new_hash[name][attribute] << property.to_s
-      end
-    end
-  end
-  new_hash
+  
+  data.each do |key, value|
+    value.each do |category, array|
+      array.each do |name|
+       pigeon_list[name][key] << category.to_S
+     end
+   end 
+ end
+    #return the newly built hash
+  pigeon_list
 end

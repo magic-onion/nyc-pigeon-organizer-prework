f10eb7bc88b497e3e4d3d92ac092a1b6830db72e
Alexander Truong <alexander.m.truong@gmail.com>
2018-10-02 18:29:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,46 @@
+require 'pry'
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  #create an array of unique names
+  pigeon_names = data.collect do |key, value|
+      value.values 
+    end
+    .flatten.uniq
+
+  pigeon_names.each {|name| new_hash[name] = {}}
+  
+  data.each do |key, value|
+    new_hash.each do |name, hash|
+      new_hash[name][key] = []
+    end
+  end
+  
+  data.each do |key, value|
+        value.each do |category, array|
+          array.each do |name|
+           new_hash[name][key] << category.to_s
+         end
+       end 
+     end
+        #return the newly built hash
+      new_hash
 end
